709a82592108b6a4853f7ec2c2f8bdcfc2a0af4e
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2023-10-17 22:23:58 +0530

    Create Find palindromes.cpp

---
+++ Find palindromes.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+
+bool ispalindrome(int k)
+{
+	if(k<10)
+	{
+		return true;
+	}
+	int original=k;
+	int reversed=0;
+	while(k!=0)
+	{
+		reversed=reversed*10+k%10;
+		k=k/10;
+	}
+
+	return original==reversed;
+
+}
+vector<int> getPalindromes(int n)
+{
+	vector<int> ans;
+	for(int i=1;i<=n;i++)
+	{
+		if(ispalindrome(i))
+		{
+			ans.push_back(i);
+		}
+	}
+
+	return ans;
+}
+
