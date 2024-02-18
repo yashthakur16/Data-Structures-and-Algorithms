ade7a1dcad71d08bb97e0c7a368daa326ac4859a
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2024-02-18 22:37:14 +0530

    Create h-index.cpp
    Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
    
    According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
    
     
    
    Example 1:
    
    Input: citations = [3,0,6,1,5]
    Output: 3
    Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
    Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
    Example 2:
    
    Input: citations = [1,3,1]
    Output: 1
     
    
    Constraints:
    
    n == citations.length
    1 <= n <= 5000
    0 <= citations[i] <= 1000

---
+++ h-index.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int hIndex(vector<int>& citations) 
+    {
+        sort(citations.begin(),citations.end(),greater<int>());
+        int h=0;
+                for(int i=0;i<citations.size();++i)
+        {
+            if(citations[i]>=i+1)
+            {
+                h=h+1;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return h;
+    }
+};
